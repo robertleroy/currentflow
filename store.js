c6de5c6a0311ba3b77f3cf78502a67b0f217ad15
Rob <robleroy@msn.com>
2019-11-14 17:33:31 -0600

    Update store.js

--- store.js
+++ store.js
@@ -1,69 +1,7 @@
-
-// import Vue from 'vue';
-// import Vuex from 'vuex';
-
-// Vue.use(Vuex);
-
-// import { States } from "./js/states";
-
-
-const Version = "0.0.1";
- 
 const store = new Vuex.Store({
   state: {
 		version: "",
-    states: [
-      { name: 'Alabama', abbr: 'al', short: 'Al' },
-      { name: 'Alaska', abbr: 'ak', short: 'Ak' },
-      { name: 'Arizona', abbr: 'az', short: 'Ariz' },
-      { name: 'Arkansas', abbr: 'ar', short: 'Ar' },
-      { name: 'California', abbr: 'ca', short: 'Ca' },
-      { name: 'Colorado', abbr: 'co', short: 'Co' },
-      { name: 'Connecticut', abbr: 'ct', short: 'Ct' },
-      { name: 'Delaware', abbr: 'de', short: 'Del' },
-      { name: 'Florida', abbr: 'fl', short: 'Fla' },
-      { name: 'Georgia', abbr: 'ga', short: 'Ga' },
-      { name: 'Hawaii', abbr: 'hi', short: 'Hi' },
-      { name: 'Idaho', abbr: 'id', short: 'Id' },
-      { name: 'Illinois', abbr: 'il', short: 'Ill' },
-      { name: 'Indiana', abbr: 'in', short: 'In' },
-      { name: 'Iowa', abbr: 'ia', short: 'Iowa' },
-      { name: 'Kansas', abbr: 'ks', short: 'Ks' },
-      { name: 'Kentucky', abbr: 'ky', short: 'Ky' },
-      { name: 'Louisiana', abbr: 'la', short: 'La' },
-      { name: 'Maine', abbr: 'me', short: 'Me' },
-      { name: 'Maryland', abbr: 'md', short: 'Md' },
-      { name: 'Massachusetts', abbr: 'ma', short: 'Mass' },
-      { name: 'Michigan', abbr: 'mi', short: 'Mich' },
-      { name: 'Minnesota', abbr: 'mn', short: 'Minn' },
-      { name: 'Mississippi', abbr: 'ms', short: 'Miss' },
-      { name: 'Missouri', abbr: 'mo', short: 'Mo' },
-      { name: 'Montana', abbr: 'mt', short: 'Mont' },
-      { name: 'Nebraska', abbr: 'ne', short: 'Ne' },
-      { name: 'Nevada', abbr: 'nv', short: 'Nv' },
-      { name: 'New Hampshire', abbr: 'nh', short: 'NH' },
-      { name: 'New Jersey', abbr: 'nj', short: 'NJ' },
-      { name: 'New Mexico', abbr: 'nm', short: 'NM' },
-      { name: 'New York', abbr: 'ny', short: 'NY' },
-      { name: 'North Carolina', abbr: 'nc', short: 'NC' },
-      { name: 'North Dakota', abbr: 'nd', short: 'ND' },
-      { name: 'Ohio', abbr: 'oh', short: 'Oh' },
-      { name: 'Oklahoma', abbr: 'ok', short: 'Ok' },
-      { name: 'Oregon', abbr: 'or', short: 'Or' },
-      { name: 'Pennsylvania', abbr: 'pa', short: 'Pa' },
-      { name: 'Rhode Island', abbr: 'ri', short: 'RI' },
-      { name: 'South Carolina', abbr: 'sc', short: 'SC' },
-      { name: 'South Dakota', abbr: 'sd', short: 'SD' },
-      { name: 'Tennessee', abbr: 'tn', short: 'Tenn' },
-      { name: 'Texas', abbr: 'tx', short: 'Tx' },
-      { name: 'Utah', abbr: 'ut', short: 'Utah' },
-      { name: 'Vermont', abbr: 'vt', short: 'Vt' },
-      { name: 'Virginia', abbr: 'va', short: 'Va' },
-      { name: 'Washington', abbr: 'wa', short: 'Wash' },
-      { name: 'West Virginia', abbr: 'wv', short: 'WV' },
-      { name: 'Wisconsin', abbr: 'wi', short: 'Wis' },
-      { name: 'Wyoming', abbr: 'wy', short: 'Wyo' }
-    ],
+    states: States,
     favorites: ['09380000'],  
     favsObj: { 
       name: 'Favorites', 
@@ -73,7 +11,7 @@ const store = new Vuex.Store({
     selectedState: null,
     graphPeriod: 7,
   },
-  
+
   getters: {    
     favorites: state => {
       return state.favorites; 
@@ -84,43 +22,33 @@ const store = new Vuex.Store({
       
       if (state.favorites.length) {        
         list.unshift(state.favsObj);
-      }
-      
+      }      
       return list;
     },
     
     selectedState: state => {
-      // console.log(typeof(state.selectedState), state.selectedState);
       if (!state.selectedState) {
         return { name: 'Colorado', abbr: 'co', short: 'Co' }
       } else {        
         return state.selectedState;
       }      
     } 
-  },
-  
-  mutations: {  
+  },  
 
-    initializeStore(state) {
+  mutations: {  
+    initializeStore(state) { 
       if ( localStorage.getItem('store') ) {
         let store = JSON.parse( 
-          localStorage.getItem('store') 
+          localStorage.getItem('store')
         );
 
-        if ( store.version === Version ) {
-					Object.assign(state, store)
-					
-					// this.replaceState(store);
-					// state.favorites = store.favorites;
-					// state.version = store.version;
-					// state.graphPeriod = store.graphPeriod
-					// state.selectedState = store.selectedState;  
+        if ( store.version == Version ) {          
+            Object.assign(state, store);
         } else {
           state.version = Version;
         }
-      }      
+      }
     },
-
 
     addFavorite (state, payload) {
       state.favorites.push(payload);
@@ -153,3 +81,4 @@ const store = new Vuex.Store({
     },
   }
 })
+
